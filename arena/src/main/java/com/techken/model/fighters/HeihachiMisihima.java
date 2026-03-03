5146668482461830d3115a94e8bed783e73cfa08
Jose Ely Perez <je206800@gmail.com>
2026-03-03 20:30:57 +0800

    Applying HeihachiMishima Stats

--- arena/src/main/java/com/techken/model/fighters/HeihachiMisihima.java
+++ arena/src/main/java/com/techken/model/fighters/HeihachiMisihima.java
@@ -1,6 +1,16 @@
 package com.techken.model.fighters;
 
-public class HeihachiMisihima {
-    //code here
-    
+import com.techken.model.BaseCharacter;
+import com.techken.skills.actions.AttackAction;
+import com.techken.skills.actions.GuardAction;
+
+public class HeihachiMisihima extends BaseCharacter {
+
+    public HeihachiMisihima() {
+        // Tank Stats: High Health (150), Low Speed (5), High Defense (25)
+        super("Heihachi Mishima", 150, 5, 25);
+        //this.skills[0] = new AttackAction("Electric Wind God Fist", 30); 
+        //this.skills[1] = new AttackAction("Stonehead", 25);              
+        //this.skills[2] = new GuardAction("Vajra Block", 20);             
+    }
 }
